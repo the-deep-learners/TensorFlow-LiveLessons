f7b7966b345b554721de47d002031e628154f255
Jon Krohn <jjpkrohn@gmail.com>
2017-05-03 20:48:38 +0000

    deleted original let_jovyan_write script

--- let_jovyan_write.sh
+++
@@ -1,3 +0,0 @@
-# script that provides jovyan, the default Docker container username, permission to write to the directory
-sudo chgrp -R 100 TensorFlow-LiveLessons/ #to recursively change group to match jovyan
-sudo chmod -R g+w TensorFlow-LiveLessons/ #to recursively change permissions so jovyan can write to the directory
